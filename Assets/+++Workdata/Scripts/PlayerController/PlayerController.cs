9330a4717c3e5a4c0a01b6322b609824fc7b9c98
Hafermilch361 <will.ronja306@gmail.com>
2025-11-03 13:27:02 +0100

    unterricht110325

--- Assets/+++Workdata/Scripts/PlayerController/PlayerController.cs
+++ Assets/+++Workdata/Scripts/PlayerController/PlayerController.cs
@@ -6,68 +6,76 @@ public class PlayerController : MonoBehaviour
 {
     #region Insepctor Variables
 
-    [SerializeField]
-    private float walkingSpeed = 5f;
-
+    [SerializeField] private float walkingSpeed;
     #endregion
 
     #region Private Variables
 
+    private Rigidbody2D _rb;
     private InputSystem_Actions _inputActions;
-    private InputAction _jumpAction;
     private InputAction _moveAction;
-    private InputAction _attackAction;
-    private InputAction _interactAction;
-    private InputAction _crouchAction;
-    private InputAction _sprintAction;
+    private InputAction _jumpAction;
 
     public Vector2 _moveInput;
-    private Rigidbody2D rb;
-    private float movementSpeed = 5f;
-    
+
+
     #endregion
-    
+
     #region Unity Event Functions
-    
+
+    /// <summary>
+    /// 
+    /// </summary>
     private void Awake()
     {
+        _rb = gameObject.GetComponent<Rigidbody2D>();
+        
         _inputActions = new InputSystem_Actions();
         _moveAction = _inputActions.Player.Move;
         _jumpAction = _inputActions.Player.Jump;
-        _attackAction = _inputActions.Player.Attack;
-        _interactAction = _inputActions.Player.Interact;
-        _crouchAction = _inputActions.Player.Crouch;
-        _sprintAction = _inputActions.Player.Sprint;
-
-        rb = GetComponent<Rigidbody2D>();
     }
+
     private void OnEnable()
     {
         _inputActions.Enable();
+
         _moveAction.performed += Move;
         _moveAction.canceled += Move;
     }
 
     private void FixedUpdate()
     {
-        rb.velocity = new Vector2(_moveInput.x * movementSpeed, rb.velocity.y
-        );
-        
+        _rb.linearVelocityX = _moveInput.x * walkingSpeed;
     }
 
     private void OnDisable()
     {
         _inputActions.Disable();
+
         _moveAction.performed -= Move;
         _moveAction.canceled -= Move;
     }
+
     #endregion
-    #region Input Methods
+
+    #region Input
 
     private void Move(InputAction.CallbackContext ctx)
     {
         _moveInput = ctx.ReadValue<Vector2>();
+
+        if (_moveInput.x > 0)
+        {
+            // right
+        }
+        else
+        {
+            //left
+        }
     }
-     
+
     #endregion
+    
+    
 }
+
